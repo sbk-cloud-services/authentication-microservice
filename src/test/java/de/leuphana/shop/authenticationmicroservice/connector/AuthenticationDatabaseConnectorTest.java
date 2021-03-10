fa53f503573a7cf2882b943e94f3cb6bc6e2d468
leoyn <leo@mixify.ga>
2021-03-10 20:56:29 +0100

    Add test for database connector

---
+++ src/test/java/de/leuphana/shop/authenticationmicroservice/connector/AuthenticationDatabaseConnectorTest.java
@@ -0,0 +1,24 @@
+package de.leuphana.shop.authenticationmicroservice.connector;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import de.leuphana.shop.authenticationmicroservice.component.structure.IncorrectCredentialsException;
+
+public class AuthenticationDatabaseConnectorTest {
+
+    private static AuthenticationDatabaseConnector authenticationDatabaseConnector;
+    
+    @BeforeAll
+    public static void setupBeforeClass() throws IncorrectCredentialsException {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationcontext.xml");
+        authenticationDatabaseConnector = (AuthenticationDatabaseConnector) applicationContext.getBean("authenticationDatabaseConnector");
+    }
+
+    @Test
+    public void canUserBeFetched() {
+        authenticationDatabaseConnector.getUser("test");
+    }
+}
